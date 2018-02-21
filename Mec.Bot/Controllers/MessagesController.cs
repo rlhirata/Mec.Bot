087e38b211c7f6f41797aec803a6a30083fe59cf
Rogério Luis Hirata <30641637+rlhirata@users.noreply.github.com>
2018-02-21 00:26:08 -0300

    Primeiro commit

---
+++ Mec.Bot/Controllers/MessagesController.cs
@@ -0,0 +1,78 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using System;
+
+namespace Mec.Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            //Configurar o EndPoint no LUIS
+            var attributes = new LuisModelAttribute(
+                ConfigurationManager.AppSettings["LuisId"],
+                ConfigurationManager.AppSettings["LuisSubscriptionKey"]);
+            var service = new LuisService(attributes);
+
+            //Colocar os '...' que está digitando
+            var reply = activity.CreateReply();
+            reply.Type = ActivityTypes.Typing;
+            reply.Text = null;
+            await connector.Conversations.ReplyToActivityAsync(reply);
+
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.MecBotDialog(service));
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Mec.Bot/Dialogs/MecBotDialog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+
+namespace Mec.Bot.Dialogs
+{
+    [Serializable]
+    public class MecBotDialog : LuisDialog<object>
+    {
+        public MecBotDialog(ILuisService service) : base(service) { }
+
+        //Não foi possível reconher a intenção
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Desculpe, mas não consegui enteder o que você disse em '{result.Query}'");
+        }
+
+        //A inteção é um cumprimento
+        [LuisIntent("Cumprimento")]
+        public async Task Cumprimento(IDialogContext context, LuisResult result)
+        {
+            var now = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time")).TimeOfDay;
+            string saudacao;
+
+            if (now > TimeSpan.FromHours(4) && now < TimeSpan.FromHours(12)) saudacao = "Bom dia";
+            else if (now < TimeSpan.FromHours(18)) saudacao = "Boa tarde";
+            else saudacao = "Boa noite";
+
+            var mensagem = $"Olá, {saudacao}! Eu sou o Mec, o assistente virtual que vai ajudá-lo a lembrar " +
+                           "quando será a próxima troca de óleo do seu veículo.";
+
+            await context.PostAsync(mensagem);
+        }
+
+        //A inteção é um sobre
+        [LuisIntent("Sobre")]
+        public async Task Sobre(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("**(▀̿Ĺ̯▀̿ ̿)** - Eu sou famoso **Bot Inteligentão**\nEstou aqui para te ajudar...");
+        }
+
+        //A inteção é um EasterEgg-Rogerio
+        [LuisIntent("EasterEgg")]
+        public async Task EasterEgg(IDialogContext context, LuisResult result)
+        {
+            var criadores = result.Entities?.Select(e => e.Entity);
+            var msgRetorno = "";
+
+            foreach (string nomeCriador in criadores)
+            {
+                switch (nomeCriador.ToLower())
+                {
+                    case "rogerio":
+                    case "rogério":
+                        msgRetorno = msgRetorno + "xiiii.... o **Rogério** é conhecido por ser 4% kkkkkk\n";
+                        break;
+                    case "anderson":
+                        msgRetorno = msgRetorno + "eita o **Anderson** já tirou foto com a Bel Pesce!!!\n";
+                        break;
+                    case "wellington":
+                        msgRetorno = msgRetorno + "cuidado porque o **Wellington** desta gente muito feliz hahahaha\n";
+                        break;
+                    case "danilo":
+                        msgRetorno = msgRetorno + "vixe o **Danilo** sempre quer dois salgados....\n";
+                        break;
+                }
+            }
+
+            msgRetorno = msgRetorno + ":)";
+            await context.PostAsync(msgRetorno);
+        }
+    }
+}
